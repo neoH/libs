f4bb0c2d35674b3fff5ee10872c4ac18d67cd0bf
huangqi <huangqi@localhost.localdomain>
2019-08-27 10:40:39 -0400

    add cpp lib

---
+++ cpp/env/options/options.cpp
@@ -0,0 +1,7 @@
+// ******************************************************************************************
+// File         : options.cpp
+// Author       : Ryan
+// Creating Date: Tue Aug 27 10:16:18 2019
+// Claim        : only the author can comment without a signature preffixed by ', that
+// means anyone else want to change the code must comments with '.
+// ******************************************************************************************
---
+++ cpp/env/options/options.h
@@ -0,0 +1,14 @@
+// ******************************************************************************************
+// File         : options.h
+// Author       : Ryan
+// Creating Date: Tue Aug 27 10:16:14 2019
+// Claim        : only the author can comment without a signature preffixed by ', that
+// means anyone else want to change the code must comments with '.
+// ******************************************************************************************
+//
+
+#ifndef options__h
+#define options__h
+
+
+#endif /* options__h */

f222fbe215c035c2e2bb49b3296103d7f201aff0
ryanH <ryanhunter30479@outlook.com>
2020-05-23 00:23:19 +0800

    up

--- cpp/env/options/main.cpp
+++
@@ -1,7 +0,0 @@
-// ******************************************************************************************
-// File         : main.cpp
-// Author       : Ryan
-// Creating Date: Tue Aug 27 10:37:25 2019
-// Claim        : only the author can comment without a signature preffixed by ', that
-// means anyone else want to change the code must comments with '.
-// ******************************************************************************************
